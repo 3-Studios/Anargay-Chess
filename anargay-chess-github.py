9f6ba8f35a6d8578df061fc77e3a2e19a7071360
Bananattttx <143480965+Bananattttx@users.noreply.github.com>
2024-02-17 18:44:30 +0000

    :3

---
+++ anargay-chess-github.py
@@ -0,0 +1,73 @@
+import discord
+import time
+import random
+from discord.ext import commands
+
+# invite me to your server: https://discord.com/api/oauth2/authorize?client_id=1208444590475051088&permissions=274877910016&scope=bot
+
+TOKEN = "TOKEN"
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix='$$', intents=intents)
+en_passant = ["google en passant", "holy hell", "new response just dropped", "actual zombie", "call the exorcist", "bishop goes on vacation, never comes back",
+           "knightmare fuel", "pawn storm incoming", "checkmate or riot", "queen sacrifice, anyone", "rook in the corner, plotting world domination",
+           "ignite the chessboard"]
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user.name}')
+    await bot.change_presence(activity=discord.Game(name=":3"))
+
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+
+    content = message.content.lower()
+
+    # reply to $brick
+    if content.startswith('$brick'):
+        time.sleep(15)
+        await message.channel.send('dont decline en passant again >:3')
+    
+    # reply to $bread
+    if content.startswith('$bread'):
+        if random.randint(1, 100) == 1:
+            time.sleep(1)
+            await message.channel.send('🍞 :3')
+        elif random.randint(1, 5) == 1:
+            time.sleep(1)
+            await message.channel.send('bread :3')
+
+    # reply to :boop:
+    if content in ['<:boop:1158242566085541959>', ':boop:', '*boop*', 'boop']:
+        await message.reply('<:boop:1158242566085541959>')
+
+    # reply to :3
+    if content == ':3':
+        await message.reply(':3')
+
+    # reply to en passant chain
+    if content in en_passant:
+        reply_value = int(en_passant.index(content)) + 1
+        if reply_value != 12:
+            await message.reply(f'{en_passant[reply_value]} :3')
+
+    await bot.process_commands(message)
+
+# $$echo [text]
+@bot.command()
+async def echo(ctx, *, arg):
+    await ctx.send(arg)
+
+# $$invite
+@bot.command()
+async def invite(ctx):
+    await ctx.send('[invite me to your server](<https://discord.com/api/oauth2/authorize?client_id=1208444590475051088&permissions=274877910016&scope=bot>)')
+
+# $$github
+@bot.command()
+async def github(ctx):
+    await ctx.send('<https://github.com/Bananattttx/Anargay-Chess>')
+
+bot.run(TOKEN)
